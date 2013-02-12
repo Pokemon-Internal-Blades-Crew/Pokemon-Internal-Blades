3dd3b9605337a14e5c773a84f1f278869adc959e
Kyle Amos <dka@anchortd.com>
2013-02-11 22:18:14 -0500

    Current Code. All works, but had to comment a line out of Move.h
    See move_h.txt for details on the commented line and the errors it
    causes when uncommented.

--- Pokemon Internal Blades/Move.h
+++ Pokemon Internal Blades/Move.h
@@ -4,24 +4,30 @@
 class Move
 {
 private:
+	std::string m_name; // name of the move
 	int m_accuracy; // base accuracy from 1 to 100. If n/a, set to 1000
 	int m_speed; // speed priority is -8 to 8 inclusive with 8 is fastest
 	int m_power; // base power.
 	int m_mp;	// How many times move can be used.
-	Pokemon target();
+	Pokemon m_target();
 public:
 	Move(void);
+	Move(std::string name, int acc, int speed, int power, int mp) 
+		: m_name(name), m_accuracy(acc), m_speed(speed), m_power(power), m_mp(mp)
+	{};
 
 	// getters
+	std::string GetName(void){return m_name;};
 	int GetAccuracy(void){return m_accuracy;};
 	int GetPriority(void){return m_speed;};
 	int GetPower(void){return m_power;};
 	int GetMP(void){return m_mp;};
 
-	void SetTarget(Pokemon target);
+	void SetTarget(Pokemon m_target);
 
-	p_Type GetTyping(Pokemon &target);
+	p_Type GetTyping(Pokemon m_target);
 
+	//Pokemon GetTarget(void){return m_target;}
 //			. Moves
 //				. Target
 //				. Typing
